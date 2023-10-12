1ec0d250695c0150b78bb17a5dc03b947cf8dea6
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-12 08:06:33 +0300

    Create 2_Statistics for  Data  Analytics cheat sheet.java

---
+++ Statistics for  Data  Analytics cheat sheet/2_Statistics for  Data  Analytics cheat sheet.java
@@ -0,0 +1,43 @@
+Data analytics involves various statistical concepts and methods. Here's a rough cheat sheet of some key concepts and their uses:
+
+**1. Descriptive Statistics:**
+These are used to summarize and understand data. Key concepts include:
+- Mean: Average of all data points.
+- Median: Middle value in an ordered list of data points.
+- Mode: Most frequently occurring value in the dataset.
+- Variance: Measure of how much values in the dataset differ from the mean.
+- Standard Deviation: Measure of the amount of variation in a set of values. A low standard deviation means the values tend to be close to the mean, while a high standard deviation means the values are spread out over a wider range [Source 4](https://www.datacamp.com/cheat-sheet/descriptive-statistics-cheat-sheet).
+
+**2. Inferential Statistics:**
+These are used to make inferences about the population based on a sample. Key concepts include:
+- Hypothesis Testing: Process of making inferences or educated guesses about a population parameter.
+- Z-Test: Used when the dataset is approximately normally distributed, and the population variance is known.
+- T-Test: Used when the dataset is approximately normally distributed, and the population variance is unknown [Source 1](https://www.stratascratch.com/blog/a-comprehensive-statistics-cheat-sheet-for-data-science-interviews/).
+
+**3. Probability:**
+This is used to predict future events based on existing data. Key concepts include:
+- Probability Distributions: These describe the likelihood of an outcome.
+- Bayes Theorem: Used to update probability predictions based on new data [Source 6](https://towardsdatascience.com/your-ultimate-data-science-statistics-mathematics-cheat-sheet-d688a48ad3db).
+
+**4. Correlation and Regression:**
+These are used to investigate the relationship between variables. Key concepts include:
+- Pearson Correlation Coefficient: Measures the linear relationship between two datasets.
+- Linear Regression: Predicts a dependent variable based on an independent variable [Source 6](https://towardsdatascience.com/your-ultimate-data-science-statistics-mathematics-cheat-sheet-d688a48ad3db).
+
+**5. Outliers:**
+These are extreme values that differ significantly from other observations. They can be caused by measurement errors, data entry errors, or other factors. Identifying and handling outliers appropriately in statistical analysis is vital to avoid inaccurate results [Source 1](https://www.stratascratch.com/blog/a-comprehensive-statistics-cheat-sheet-for-data-science-interviews/).
+
+**6. Chi-squared Test:**
+This is used to determine if there is a significant association between two categorical variables. For example, it can be used to determine if there is an association between a customer's age group and their preferred product category [Source 1](https://www.stratascratch.com/blog/a-comprehensive-statistics-cheat-sheet-for-data-science-interviews/).
+
+**7. Central Limit Theorem:**
+This states that if you have a large enough sample size, the distribution of the sample means will be approximately normally distributed, regardless of the shape of the population distribution [Source 1](https://www.stratascratch.com/blog/a-comprehensive-statistics-cheat-sheet-for-data-science-interviews/).
+
+Remember to always visualize your data, as it can help you better understand patterns, trends, and relationships in your dataset. Tools like `ggplot2` and `Tableau` are commonly used for this purpose [Source 0](https://www.thinkful.com/blog/data-analyst-cheat-sheet/).
+  
+Note: This is a high-level overview and there are many more concepts and details in statistics for data analytics. Depending on your specific needs and the complexity of your data, you may need to delve deeper into these topics.
+
+
+
+
+....
