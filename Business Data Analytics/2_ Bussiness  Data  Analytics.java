e91955c1ee35f1d4a48cc11adad72b0f13f41ef3
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-12 08:14:35 +0300

    Create 2_ Bussiness  Data  Analytics.java

---
+++ Business Data Analytics/2_ Bussiness  Data  Analytics.java
@@ -0,0 +1,37 @@
+Business Data Analytics is a broad field that involves various processes, tools, and concepts. Here's a brief cheat sheet that covers some key areas:
+
+**1. Descriptive, Predictive, and Prescriptive Analytics:**
+
+Descriptive analytics involves analyzing historical data to understand what has happened. Predictive analytics involves using statistical models and forecasting techniques to understand the future. Prescriptive analytics involves using optimization and simulation algorithms to advise on possible outcomes [Source 6](https://www.techrepublic.com/article/prescriptive-analytics-a-cheat-sheet/).
+
+**2. Google Analytics, Data Studio & Trends:**
+
+These tools are crucial for understanding website traffic, user behavior, and trends. They can help you make data-driven decisions to improve your website's performance and customer engagement [Source 0](https://www.thinkful.com/blog/data-analyst-cheat-sheet/).
+
+**3. SQL:**
+
+Structured Query Language (SQL) is a standard language for managing and manipulating databases. Knowing SQL is essential for any data analyst as it allows them to retrieve, update, and manipulate data stored in databases [Source 8](https://www.kdnuggets.com/2022/02/complete-collection-data-science-cheat-sheets-part-1.html).
+
+**4. Probability and Statistics:**
+
+Understanding probability and statistics is fundamental to data analytics. Key concepts include hypothesis testing, correlation, regression, probability distributions, and statistical significance [Source 0](https://www.thinkful.com/blog/data-analyst-cheat-sheet/).
+
+**5. Data Visualization:**
+
+Data visualization is the graphical representation of data. It involves producing images that communicate relationships among the represented data to viewers of the images. Tools like Tableau and PowerBI are commonly used for this purpose [Source 9](https://medium.com/@reachtoanamikasingh19/a-comprehensive-collection-of-data-analysis-cheat-sheets-for-2023-3e1f66a9a900).
+
+**6. Machine Learning:**
+
+Machine learning is a method of data analysis that automates analytical model building. It involves algorithms that iteratively learn from data, allowing computers to find hidden insights without being explicitly programmed where to look [Source 9](https://medium.com/@reachtoanamikasingh19/a-comprehensive-collection-of-data-analysis-cheat-sheets-for-2023-3e1f66a9a900).
+
+**7. Big Data Tools:**
+
+Big data tools like Hadoop, Spark, and Hive are essential for storing, processing, and analyzing large datasets. These tools allow data analysts to work with large datasets that traditional databases can't handle [Source 8](https://www.kdnuggets.com/2022/02/complete-collection-data-science-cheat-sheets-part-1.html).
+
+Note: This is a high-level overview and there are many more concepts and details in business data analytics. Depending on your specific needs and the complexity of your data, you may need to delve deeper into these topics.
+
+
+
+
+
+....
