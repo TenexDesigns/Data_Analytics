e93d6beba35c10471cbce94a016917d65da03685
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-12 08:05:44 +0300

    Create 1_ Statistics for  Data  Analytics cheat sheet.java

---
+++ Statistics for  Data  Analytics cheat sheet/1_ Statistics for  Data  Analytics cheat sheet.java
@@ -0,0 +1,81 @@
+Creating a cheat sheet for statistics in data analytics can be quite comprehensive, but I'll provide you with a condensed version that covers some of the fundamental concepts and formulas frequently used in this field. Please note that this is not an exhaustive list, and the depth of your cheat sheet may vary based on your specific needs and level of expertise. Here's a basic cheat sheet:
+
+### Descriptive Statistics:
+1. **Mean (Average):**
+   - Formula: \(\bar{x} = \frac{\sum_{i=1}^{n}x_i}{n}\)
+
+2. **Median (Middle Value):**
+   - For odd-sized datasets: \(Median = \text{Value at}\ \frac{n+1}{2}\)
+   - For even-sized datasets: \(Median = \frac{x_{n/2} + x_{(n/2)+1}}{2}\)
+
+3. **Mode (Most Common Value):**
+   - The value(s) that occur most frequently.
+
+4. **Range:**
+   - Range = Max - Min
+
+5. **Variance:**
+   - Formula: \(Var(X) = \frac{\sum_{i=1}^{n}(x_i - \bar{x})^2}{n-1}\)
+
+6. **Standard Deviation:**
+   - Formula: \(SD(X) = \sqrt{Var(X)}\)
+
+### Probability:
+7. **Probability (P):**
+   - The likelihood of an event occurring.
+   - Range: 0 ≤ P ≤ 1
+
+8. **Joint Probability:**
+   - Probability of two or more events occurring together.
+
+### Distributions:
+9. **Normal Distribution:**
+   - Characterized by mean (μ) and standard deviation (σ).
+   - 68-95-99.7 rule (Empirical Rule).
+
+10. **Binomial Distribution:**
+    - For discrete, binary outcomes.
+    - Formula: \(P(X = k) = \binom{n}{k} p^k (1-p)^{n-k}\)
+
+11. **Poisson Distribution:**
+    - For rare events with constant mean rate.
+    - Formula: \(P(X = k) = \frac{e^{-λ}λ^k}{k!}\)
+
+### Inferential Statistics:
+12. **Hypothesis Testing:**
+    - Null Hypothesis (\(H_0\)) vs. Alternative Hypothesis (\(H_1\) or \(H_a\)).
+    - Common tests: t-test, chi-squared test, ANOVA, etc.
+
+13. **p-value:**
+    - Probability of observing a test statistic as extreme as, or more extreme than, the one calculated from the sample data under the null hypothesis.
+
+14. **Confidence Interval:**
+    - Range within which a population parameter is estimated to lie.
+    - Common confidence levels: 90%, 95%, 99%.
+
+15. **Regression Analysis:**
+    - Linear Regression, Logistic Regression, etc.
+
+### Data Visualization:
+16. **Histogram:**
+    - Visual representation of the distribution of a dataset.
+
+17. **Box Plot (Box-and-Whisker Plot):**
+    - Display of the data's distribution and outliers.
+
+18. **Scatter Plot:**
+    - Depicts the relationship between two variables.
+
+19. **Correlation Coefficient (Pearson's r):**
+    - Measures the strength and direction of a linear relationship between two variables.
+
+20. **Coefficient of Determination (R-squared):**
+    - Indicates the proportion of the variance in the dependent variable explained by the independent variable.
+
+This cheat sheet provides a starting point for understanding and working with statistics in data analytics. Depending on your specific needs and projects, you may want to delve deeper into certain topics and formulas.
+
+
+
+
+
+  ....
